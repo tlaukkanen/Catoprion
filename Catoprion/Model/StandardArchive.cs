8a143d843350a089d05919732bbec24340e764e5
Tommi Laukkanen <tommi.laukkanen@elisa.fi>
2020-06-23 17:21:53 +0300

    Add proper archive type

--- Catoprion/Model/StandardArchive.cs
+++ Catoprion/Model/StandardArchive.cs
@@ -1,7 +1,9 @@
+using Piranha.AttributeBuilder;
 using Piranha.Models;
 
 namespace Catoprion
 {
+    [PageType(Title="Standard archive", IsArchive=true)]
     public class StandardArchive : Page<StandardArchive>
     {
         public PostArchive<PostInfo> Archive { get; set; }
